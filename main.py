173d6288fc1986cef5138d7624b6aad3a04c088b
Alina Kozyreva <alina0206200119@gmail.com>
2021-05-19 13:39:25 +0300

    create ability to put arguments

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import sys
-def print_hi(name):
+import requests
+import argparse
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+parser = argparse.ArgumentParser()
+parser.add_argument("--json", default="results.json")
+parser.add_argument("-q", "--query", required=True)
+parser.parse_args()
+print(parser.parse_args())
 
+link = 'https://e-dostavka.by/'
+response = requests.get(link).status_code
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(response)
 
