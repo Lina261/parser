1e69a3a481f5976014fe593253b1f3a2acaa3ea6
Alina Kozyreva <alina0206200119@gmail.com>
2021-05-19 14:00:12 +0300

    add arguments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ parser.add_argument("--json", default="results.json")
 parser.add_argument("-q", "--query", required=True)
 parser.parse_args()
 print(parser.parse_args())
+print('Welcome to program!')
 
 link = 'https://e-dostavka.by/'
 response = requests.get(link).status_code
